fdf6f42d6b1b27e67d7e3ece33b21a9338d7ee16
Ryan Rickgauer <rrickgauer1@gmail.com>
2023-03-29 11:14:22 -0500

    update functions

--- weather/main.py
+++ weather/main.py
@@ -1,33 +1,10 @@
-
-
-
-from pyowm import OWM
-from pyowm.weatherapi25.weather import Weather
-from pyowm.utils import config
-from pyowm.utils import timestamps
+import routines as weather_routines
 import json
 
-import secrets as mysecrets
 
-# ---------- FREE API KEY examples ---------------------
+def test():
+    current_weather = weather_routines.get_weather_by_zip('60614')
+    print(json.dumps(current_weather.to_dict(), indent=4))
 
-owm = OWM(mysecrets.WEATHER_API_KEY)
-mgr = owm.weather_manager()
-
-
-# Search for current weather in London (Great Britain) and get details
-observation = mgr.weather_at_place('London,GB')
-w: Weather = observation.weather
-
-w.detailed_status         # 'clouds'
-w.wind()                  # {'speed': 4.6, 'deg': 330}
-w.humidity                # 87
-w.temperature('celsius')  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
-w.rain                    # {}
-w.heat_index              # None
-w.clouds                  # 75
-
-
-print(json.dumps(w.__dict__, indent=4))
-
+test()
 
--- weather/routines.py
+++ weather/routines.py
@@ -1,16 +1,23 @@
-
+from __future__ import annotations
 from pyowm import OWM
 from pyowm.weatherapi25.weather import Weather
+from pyowm.weatherapi25.weather_manager import WeatherManager
 import secrets as mysecrets
 
 
 def get_weather_by_zip(zipcode: str) -> Weather:
-    """
-    Get the current weather by the specified zip code
-    """
+    """Get the current weather by the specified zip code"""
+    
+    weather_manager = _get_weather_manager()
+    observation = weather_manager.weather_at_zip_code(zipcode, 'US')
+
+    return observation.weather
+
+
+def _get_weather_manager() -> WeatherManager:
+    """Get a new weather manager object"""
+
     owm = OWM(mysecrets.WEATHER_API_KEY)
     mgr = owm.weather_manager()
 
-    observation = mgr.weather_at_zip_code(zipcode)
-
-    return observation.weather
+    return mgr
